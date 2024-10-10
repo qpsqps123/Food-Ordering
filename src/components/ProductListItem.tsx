e14b4f753ec3cf3b173e9f654c60a9008b1772ac
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2024-10-10 17:55:34 +0900

    [Fix] 타입 파일 추가 및 이에 따른 타입 오류 수정

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,18 +1,21 @@
+import Colors from "@/constants/Colors";
+import { Product } from "@/types";
 import { Image, StyleSheet, Text, View } from "react-native";
-import Colors from "../constants/Colors";
 
-const ProductListItem = ({
-  product,
-}: {
-  product: {
-    name: string;
-    image: string;
-    price: string;
-  };
-}) => {
+type TProductListItem = {
+  product: Product;
+};
+
+const defaultPizzaImage =
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
+
+const ProductListItem = ({ product }: TProductListItem) => {
   return (
     <View style={styles.container}>
-      <Image source={{ uri: product.image }} style={styles.image} />
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        style={styles.image}
+      />
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>{product.price}</Text>
     </View>
