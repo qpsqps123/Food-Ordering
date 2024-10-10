394e33d3fe9065b834406ab3318f5049ea914e1a
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2024-10-10 17:56:26 +0900

    [Refactor] 파일 경로 가독성 향상

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,3 +1,4 @@
+import { useColorScheme } from "@components/useColorScheme.web";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import {
   DarkTheme,
@@ -9,8 +10,6 @@ import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import "react-native-reanimated";
-
-import { useColorScheme } from "@/src/components/useColorScheme";
 
 export {
   // Catch any errors thrown by the Layout component.
